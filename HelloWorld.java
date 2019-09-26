16ce2ab2a7b4ab1f830934dcf707d010ec532f69
Anant Singh <anantsingh017@gmail.com>
2019-09-26 17:31:49 -0400

    testing1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@ class HelloWorld {
   public static void main(String args[]) {
     System.out.println("Hello World!");
   }
+  public static void hello() {
+  	System.out.println("Hello");
+  }
 }
 
 // Testing
